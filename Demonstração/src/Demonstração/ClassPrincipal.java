c50a86b3a96d643f8a73eb1072b27c8bb4f50a9e
Your Name <you@example.com>
2021-09-23 16:33:43 -0300

    Métodos com Parâmetros Java

---
+++ Demonstração/src/Demonstração/ClassPrincipal.java
@@ -0,0 +1,22 @@
+package Demonstração;
+
+public class ClassPrincipal {
+
+	public static void main(String[] args) {
+//		
+//		int numero = 1;
+//		float decimal = 10.4f;
+//		String nome = "Ednêi";
+//		boolean verdade = true;
+//		boolean mentira = false;
+		
+		Exemplos metodos = new Exemplos();
+		
+		metodos.somar();
+		metodos.multiplicar();
+			
+	}
+	
+	
+
+}
---
+++ Demonstração/src/Demonstração/Exemplos.java
@@ -0,0 +1,23 @@
+package Demonstração;
+
+public class Exemplos {
+
+			public void somar () {
+			int numero1 = 5;
+			int numero2 = 7;
+			
+			int resultado = numero1 + numero2;
+			System.out.println(resultado);
+			
+		}
+			
+			public void multiplicar () {
+				int numero1 = 5;
+				int numero2 = 7;
+				
+				int resultado = numero1 * numero2;
+				System.out.println(resultado);
+				
+			}
+		
+}
