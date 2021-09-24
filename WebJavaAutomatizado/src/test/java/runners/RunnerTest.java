e4c020af920c81099c8593e6ec18262e756ad805
Your Name <you@example.com>
2021-09-23 23:11:10 -0300

    Login e não login testes rodando

--- WebJavaAutomatizado/src/test/java/runners/RunnerTest.java
+++ WebJavaAutomatizado/src/test/java/runners/RunnerTest.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 		plugin = {"pretty"},
 		features = "src/test/resources/features",
 		glue=  "stepsDefinitions",
-		tags = "@loginSucesso",
+		tags = "@login",
 		snippets = SnippetType.CAMELCASE,
 		monochrome = true,
 		dryRun = false	
--- WebJavaAutomatizado/src/test/java/stepsDefinitions/LoginSteps.java
+++ WebJavaAutomatizado/src/test/java/stepsDefinitions/LoginSteps.java
@@ -1,5 +1,11 @@
 package stepsDefinitions;
 
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindAll;
+
+import static utils.Utils.*;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 import pageObjects.LoginPage;
@@ -25,14 +31,13 @@ public class LoginSteps {
 
 	@Entao("o sistema exibe a mensagem usuario invalido")
 	public void oSistemaExibeAMensagemUsuarioInvalido() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		  assertEquals("Invalid credentials", driver.findElement(By.id("spanMessage")).getText());
 	}
 
 	@Entao("o sistema exibe o usuario logado")
 	public void oSistemaExibeOUsuarioLogado() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+//		assertEquals("welcome Ram", driver.findElement(By.id("welcome").getText());
+	  assertEquals("Welcome Ram", driver.findElement(By.id("welcome")).getText());
 	}
 
 }
